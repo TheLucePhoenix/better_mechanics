66ba71cac40677979d7b3be36f1befb220eb75c4
Raff727 <108025951+Raff727@users.noreply.github.com>
2022-06-24 22:10:03 +0200

    Added Logger

--- src/main/java/de/thekitsunephoenix/better_mechanics/Better_mechanics.java
+++ src/main/java/de/thekitsunephoenix/better_mechanics/Better_mechanics.java
@@ -1,14 +1,24 @@
 package de.thekitsunephoenix.better_mechanics;
 
+import de.thekitsunephoenix.better_mechanics.block.ModBlocks;
 import de.thekitsunephoenix.better_mechanics.item.ModItems;
 import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Better_mechanics implements ModInitializer {
 
     public static String MOD_ID = "better_mechanics";
 
+    public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
+
     @Override
     public void onInitialize() {
         ModItems.registerModItems();
+        ModBlocks.registerModBlocks();
+
+
+
+
     }
 }
