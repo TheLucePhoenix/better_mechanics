f284ceb7596c6d8335254cc8a62f690ba72c730b
TheLucePhoenix <108028610+TheLucePhoenix@users.noreply.github.com>
2022-06-24 21:54:39 +0200

    Created more ModToolMaterials and changed values.

--- src/main/java/de/thekitsunephoenix/better_mechanics/item/ModToolMaterials.java
+++ src/main/java/de/thekitsunephoenix/better_mechanics/item/ModToolMaterials.java
@@ -2,30 +2,29 @@ package de.thekitsunephoenix.better_mechanics.item;
 
 import de.thekitsunephoenix.better_mechanics.util.ModTags;
 import net.minecraft.item.ItemConvertible;
-import net.minecraft.item.Items;
 import net.minecraft.item.ToolMaterial;
 import net.minecraft.recipe.Ingredient;
 import net.minecraft.util.Lazy;
 import java.util.function.Supplier;
 
     public enum ModToolMaterials implements ToolMaterial {
-        COPPER(0, 48, 1.8F, 0.0F, 12, () -> {
+        COPPER(0, 48, 1.8F, 0.0F, 11, () -> {
             return Ingredient.fromTag(ModTags.Items.COPPER_INGOTS);
         }),
-        MYTHRIL(2, 220, 6.5F, 2.8F, 12, () -> {
-            return Ingredient.ofItems(new ItemConvertible[]{ModItems.MYTHRIL_INGOT});
+        STEEL(2, 561, 5.0F, 2.0F, 12, () -> {
+            return Ingredient.ofItems(new ItemConvertible[]{ModItems.STEEL_INGOT});
         }),
-        TITANIUM(4, 2327, 8.0F, 3.5F, 17, () -> {
+        SAPPHIRE(2, 137, 14.0F, 5.0F, 22, () -> {
+            return Ingredient.ofItems(new ItemConvertible[]{ModItems.SAPPHIRE_REFINED});
+        }),
+        MITHRIL(3, 735, 9.75F, 2.8F, 18, () -> {
+            return Ingredient.ofItems(new ItemConvertible[]{ModItems.MITHRIL_INGOT});
+        }),
+        TITANIUM(4, 2327, 8.5F, 3.5F, 17, () -> {
             return Ingredient.ofItems(new ItemConvertible[]{ModItems.TITANIUM_INGOT});
         }),
-        DIAMOND(3, 1561, 8.0F, 3.0F, 10, () -> {
-            return Ingredient.ofItems(new ItemConvertible[]{Items.DIAMOND});
-        }),
-        GOLD(0, 32, 12.0F, 0.0F, 22, () -> {
-            return Ingredient.ofItems(new ItemConvertible[]{Items.GOLD_INGOT});
-        }),
-        NETHERITE(4, 2031, 9.0F, 4.0F, 15, () -> {
-            return Ingredient.ofItems(new ItemConvertible[]{Items.NETHERITE_INGOT});
+        TUNGSTEN(5, 2731, 6.5F, 4.0F, 15, () -> {
+            return Ingredient.ofItems(new ItemConvertible[]{ModItems.TUNGSTEN_INGOT});
         });
 
         private final int miningLevel;
